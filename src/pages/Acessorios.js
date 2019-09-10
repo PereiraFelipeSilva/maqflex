d01079a538e0dc89cd749d555d519c3b14bb16db
Felipe Pereira <pereira_felipesilva@outlook.com>
2019-09-10 11:52:07 -0300

    finalização da página acessórios

--- src/pages/Acessorios.js
+++ src/pages/Acessorios.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import '../css/Acessorios.css';
+import acessorios from '../assets/acessorios-flexografia.jpg';
 
 export default function Acessorios() {
 
   return (
     <>
       <Header />
-      <p>
-        A MAQ FLEX oferece uma linha completa de peças e acessórios para equipamentos do segmento flexográfico. Consulte-nos para tamanhos especiais.
-      </p>
+      <main>
+        <section className="secao-acessorios" >
+          <p>
+            A MAQ FLEX oferece uma linha completa de peças e acessórios <br /> para equipamentos do segmento flexográfico. <br /> Consulte-nos para <span>tamanhos especiais</span>.
+          </p>
+        </section>
+
+        <section className="secao-acessorios" >
+          <img src={acessorios} alt="Acessórios" />
+        </section>
+
+        <section className="secao-acessorios" >
+          <p>
+            Cilindros Porta Clichês, Cilindros Anilox, Cilindros Pescadores, Cilindros Contra Pressão, Cilindros Contra Facas, Engrenagens, Sistema de Doctor Blade, Mandris Expansivos Pneumático, Mandris Expansivos Mecânico, Feltros, Correias, Rolamentos, entre outros.
+          </p>
+        </section>
+      </main>
       <Footer />
     </>
   );
